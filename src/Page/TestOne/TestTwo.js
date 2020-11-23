59504271e92d5a6e53340ec6bc1851d6cde10dd5
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2020-11-23 18:11:24 +0800

    add setTimeout and interval.

--- src/Page/TestOne/TestTwo.js
+++ src/Page/TestOne/TestTwo.js
@@ -6,25 +6,54 @@ import './index.css'
         * constructor(props) {
         * super(props);
         */
+       constructor(props){
+           super(props) 
+               this.state={
+                   val:0,
+                   date:new Date()
+               }
+        }
     /**
      * define life cycle 
      * 
      */
     componentDidMount(){
         console.log('i am a componentDidMount')
+        this.setState({val:this.state.val+1})
+        console.log(this.state.val);
+        this.timer=setTimeout(() => {
+            console.log(this.state.val);
+        }, 1000);
+        this.timerId=setInterval(
+            () => this.tick(),
+            1000
+        );
     }
     componentDidUpdate(){
         console.log('i am  a componentDidUpdate');
     }
     componentWillUnmount(){
         console.log('i am a componentWillUnmount');
+        clearTimeout(this.timer)
+        clearInterval(this.timerId)
     }
+    tick(){
+        this.setState({
+            date:new Date()
+        });
+    }
+    /**
+     * define function
+     * 
+     */
     /**
      * render and do some layout
      */
     render(){ 
         return(
             <div className='divTwo'>
+                <h1>{this.state.date.toLocaleTimeString()}</h1>
+                <h1>{this.state.val}</h1>
                  <h1>222</h1>
                 <Button
                 type="primary" shape="round"
